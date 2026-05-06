de408aa7b97da508b0bda9ee2b260fd7236770f6
Bhavishyakumar2006 <bhavishyakumar32c@gmail.com>
2026-05-06 13:48:26 +0530

    work on hobby tab

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ let skillTab = document.querySelector("#skill-tab");
 let skillContainer = document.querySelector('#skill-container'); 
 let educationTab = document.querySelector('#education-tab'); 
 let educationContent = document.querySelector('#education-content');
+let hobbyTab = document.querySelector('#hobby-tab');
+let hobbyContainer = document.querySelector('#hobby-container');
 console.log(skillContainer);
 
 skillTab.addEventListener('click', () =>{
     skillContainer.style.display="block";
     educationContent.style.display="none";
+    hobbyContainer.style.display="none";
 });
 educationTab.addEventListener('click', () =>{
-    skillContainer.style.display="none";
     educationContent.style.display="block";
-    
-})
+    skillContainer.style.display="none";
+    hobbyContainer.style.display="none";
+});
+hobbyTab.addEventListener('click', () =>{
+    hobbyContainer.style.display="block";
+    skillContainer.style.display="none";
+    educationContent.style.display="none";
+});
